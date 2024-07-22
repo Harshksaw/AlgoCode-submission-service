375254b39fe41ceb9e0e7bb676307e254c283b75
Harsh kumar <mister.harshkumar@gmail.com>
2024-07-22 10:56:38 +0530

    chore: Add Fastify server, API routes, and plugins for todo application

--- todo/index.js
+++ todo/index.js
@@ -1,22 +1,39 @@
-const fastify = require('fastify')({ logger: true });// Create a new Fastify instance
+const fastify = require("fastify")({ logger: true }); // Create a new Fastify instance
 
-
-fastify.addHook('onReady', (done) => {
-    console.log('App is ready');
-    done();
+fastify.addHook("onReady", (done) => {
+  console.log("App is ready");
+  done();
 });
 const port = process.env.PORT || 3000;
 
-
 async function start() {
-    try {
-        await fastify.listen(port);
-        fastify.log.info(`server listening on ${fastify.server.address().port}`);
-
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+  try {
+    await fastify.listen(port);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
 
+
+
 start();
+
+function samplePlugin(fastify, opts, done) {
+    console.log("excuting my plugin");
+    fastify.decorate("key", "value"); //atach own keyvalue apirs to fastify instance
+    console.log(fastify);
+    done();
+  }
+  
+  fastify.register(samplePlugin, { message: "hello" });
+
+fastify.route({
+  url: "/route",
+  method: "GET",
+  handler: async (request, reply) => {
+    return { hello: "world" };
+  },
+});
+
